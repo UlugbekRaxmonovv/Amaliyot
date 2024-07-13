1094a0d42387669afa98eb9ae44f4e7f351442e0
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2024-07-13 19:46:29 +0500

    main

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // images:{
-    //     domains: ['encrypted-tbn0.gstatic.com','www.svgrepo.com','upload.wikimedia.org','encrypted-tbn0.gstatic.com',
-    //     'cdn.worldvectorlogo.com','www.svgrepo.com','upload.wikimedia.org','static-00.iconduck.com',
-    //     'encrypted-tbn0.gstatic.com','upload.wikimedia.org','upload.wikimedia.org','images.crunchbase.com','static-00.iconduck.com',
-    //     'www.google.com','encrypted-tbn0.gstatic.com','encrypted-tbn0.gstatic.com',
-    //     'static-00.iconduck.com','encrypted-tbn0.gstatic.com','cdn.iconscout.com','upload.wikimedia.org','jmeter.apache.org','asset.brandfetch.io',
-    //     'encrypted-tbn0.gstatic.com','upload.wikimedia.org','encrypted-tbn0.gstatic.com','cdn.inspireuplift.com',
-    //     'encrypted-tbn0.gstatic.com','cdn.worldvectorlogo.com','cdn.freebiesupply.com','encrypted-tbn0.gstatic.com','www.svgrepo.com',
-    //     'encrypted-tbn0.gstatic.com','encrypted-tbn0.gstatic.com','www.svgrepo.com','encrypted-tbn0.gstatic.com','encrypted-tbn0.gstatic.com',
-    //     'static-00.iconduck.com','www.svgrepo.com','encrypted-tbn0.gstatic.com','encrypted-tbn0.gstatic.com','static-00.iconduck.com',
-    //     'encrypted-tbn0.gstatic.com','upload.wikimedia.org','encrypted-tbn0.gstatic.com','encrypted-tbn0.gstatic.com',
-    //     'encrypted-tbn0.gstatic.com','upload.wikimedia.org','static-00.iconduck.com','upload.wikimedia.org'
+    images:{
+        domains: ['encrypted-tbn0.gstatic.com','www.svgrepo.com','upload.wikimedia.org','encrypted-tbn0.gstatic.com',
+        'cdn.worldvectorlogo.com','www.svgrepo.com','upload.wikimedia.org','static-00.iconduck.com',
+        'encrypted-tbn0.gstatic.com','upload.wikimedia.org','upload.wikimedia.org','images.crunchbase.com','static-00.iconduck.com',
+        'www.google.com','encrypted-tbn0.gstatic.com','encrypted-tbn0.gstatic.com',
+        'static-00.iconduck.com','encrypted-tbn0.gstatic.com','cdn.iconscout.com','upload.wikimedia.org','jmeter.apache.org','asset.brandfetch.io',
+        'encrypted-tbn0.gstatic.com','upload.wikimedia.org','encrypted-tbn0.gstatic.com','cdn.inspireuplift.com',
+        'encrypted-tbn0.gstatic.com','cdn.worldvectorlogo.com','cdn.freebiesupply.com','encrypted-tbn0.gstatic.com','www.svgrepo.com',
+        'encrypted-tbn0.gstatic.com','encrypted-tbn0.gstatic.com','www.svgrepo.com','encrypted-tbn0.gstatic.com','encrypted-tbn0.gstatic.com',
+        'static-00.iconduck.com','www.svgrepo.com','encrypted-tbn0.gstatic.com','encrypted-tbn0.gstatic.com','static-00.iconduck.com',
+        'encrypted-tbn0.gstatic.com','upload.wikimedia.org','encrypted-tbn0.gstatic.com','encrypted-tbn0.gstatic.com',
+        'encrypted-tbn0.gstatic.com','upload.wikimedia.org','static-00.iconduck.com','upload.wikimedia.org'
     
-    // ]
-    // }
+    ]
+    }
 };
 
 export default nextConfig;
