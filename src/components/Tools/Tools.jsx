076c5b0de39814a5d9f36b63047e5af89ba86582
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2024-07-13 19:42:31 +0500

    main

--- src/components/Tools/Tools.jsx
+++ src/components/Tools/Tools.jsx
@@ -1,28 +1,28 @@
 
-// import React from 'react';
-// import './Tools.scss'
-// import {getData }from '../../components/fetch/index'
-// import Skills from '../Skills/Skills';
-// const  Tools = async () => {
-//   let data =   await getData("Sneakers")
-//   let category =   await getData("category")
+import React from 'react';
+import './Tools.scss'
+import {getData }from '../../components/fetch/index'
+import Skills from '../Skills/Skills';
+const  Tools = async () => {
+  let data =   await getData("Sneakers")
+  let category =   await getData("category")
   
-//   console.log(category);
-//     return (
-//        <>
-//        <section id='Tools'>
-//         <div className="container">
-//         <div className="h1 ">
-//             <h1>Tools</h1>
-//         </div>
-//         <Skills data={data} category={category}/>
+  console.log(category);
+    return (
+       <>
+       <section id='Tools'>
+        <div className="container">
+        <div className="h1 ">
+            <h1>Tools</h1>
+        </div>
+        <Skills data={data} category={category}/>
        
        
-//         </div>
+        </div>
        
-//        </section>
-//        </>
-//     );
-// }
+       </section>
+       </>
+    );
+}
 
-// export default Tools;
+export default Tools;
